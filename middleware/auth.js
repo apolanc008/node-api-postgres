6e66f61a3ff18fbc3d27a24e67d47708f6fb243a
APOLANC008 <ambar.polanco@lc.cuny.edu>
2025-03-24 19:26:03 -0400

    admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,3 +7,11 @@ passport.use(new LocalStrategy(
         // Call done(null, user) if authentication is successful
     }
 ));
+
+function isAdmin(req, res, next) {
+    if (req.user && req.user.role === 'admin') {
+        return next();
+    } else {
+        return res.status(403).json({ message: 'Permission denied' });
+    }
+}
